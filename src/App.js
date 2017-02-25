b113a77825ad92d4e2b07bd63d19490b3e9e1461
Matan Sokolovsky <matanso@gmail.com>
2017-02-25 16:45:58 +0200

    Deploy test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Testing app
+          Another test
         </p>
       </div>
     );
